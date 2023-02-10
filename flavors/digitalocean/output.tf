ccb235e7a6b938a893846544eb8660b8aa3adbf0
Boris Aguilar <baguilar@innovare.es>
2023-02-09 22:36:08 -0600

    Adding bugfix bad naminig in output

--- flavors/digitalocean/output.tf
+++
@@ -1,9 +0,0 @@
-output "ip_address" {
-  description = "IP Address of deployed server"
-  value       = digitalocean_droplet.www-nocodb.ipv4_address
-}
-
-output "http_address" {
-  description = "You can visit your installation at: "
-  value       = "http://${var.domain != "" ? var.domain : digitalocean_droplet.www-nocodb.ipv4_address}/"
-}
